7a9ec413d257cc437f07a5945e5013b1e5c78859
sravanidasari4488 <154404196+sravanidasari4488@users.noreply.github.com>
2026-02-17 21:20:21 +0530

    Delete Desktop directory

--- Desktop/userapp/frontend/src/components/common/AppTextInput.jsx
+++
@@ -1,6 +0,0 @@
-import { TextInput } from 'react-native';
-
-export default function AppTextInput(props) {
-  const { style, ...rest } = props;
-  return <TextInput {...rest} style={[{ fontFamily: 'Poppins_400Regular' }, style]} />;
-}
